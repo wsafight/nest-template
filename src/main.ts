c329c4d8cf9be3a9311e9dbf70312897a46e6591
wsafight <984292420@qq.com>
2023-06-22 14:48:35 +0800

    #FEAT: add docs

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,20 @@ import compress from '@fastify/compress';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
-
 import { AppModule } from './app.module';
 
+/** 默认配置 3000 端口 */
+const API_DEFAULT_PORT = 3000;
+/** 默认配置请求前缀 */
+const API_DEFAULT_PREFIX = '/api';
+
 async function bootstrap() {
+  // 使用 Fastify 提升性能
   const adapter: FastifyAdapter = new FastifyAdapter({ logger: true });
 
+  // 安全防护
   adapter.register(helmet);
+  // 压缩响应
   adapter.register(compress);
 
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -23,15 +30,25 @@ async function bootstrap() {
     adapter,
   );
 
+  // 设置请求前缀
+  app.setGlobalPrefix(process.env.API_PREFIX || API_DEFAULT_PREFIX);
+
+  // 开启版本管理
   app.enableVersioning({
     defaultVersion: '1',
     type: VersioningType.URI,
   });
 
+  // 设置全局返回
   app.useGlobalInterceptors(new TransformInterceptor());
 
+  // 抛出错误
   app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
 
-  await app.listen(3000);
+  await app.listen(process.env.API_PORT || API_DEFAULT_PORT);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
